2dbe56a98560df5a0e49feb39cf846091cc05753
Jack <darkangelhackerjack@gmail.com>
2025-04-02 10:21:23 +0100

    Have added class TodoItem, Implemented 2 functions: Do task{marks task as doen}, completed task[prints completed task and says their number], modified all functions to be able to work with the class, modified display tasks so it shows all tasks and wheter they have been completed or not

--- To-Do List/Program.cs
+++ To-Do List/Program.cs
@@ -1,10 +1,12 @@
 // See https://aka.ms/new-console-template for more information
-LinkedList<string> tasks = new LinkedList<string>();
+using To_Do_List;
+
+LinkedList<TodoItem> tasks = new LinkedList<TodoItem>();
 
 while (true)
 {
     byte choice;
-    Console.WriteLine("[1] Add Task, [2] View Tasks, [3] Remove Task, [0] Exit");
+    Console.WriteLine("[1] Add Task, [2] View Tasks, [3] Remove Task, [4] Do Task, [5] Completed Tasks, [0] Exit");
     try { choice = byte.Parse(Console.ReadLine()); }
     catch { Console.WriteLine("Invalid choice"); continue; }
     if (choice == 0)
@@ -24,28 +26,36 @@ while (true)
     {
         RemoveTask(tasks);
     }
+    else if (choice == 4)
+    {
+        DoTask(tasks);
+    }
+    else if (choice == 5)
+    {
+        CompletedTasks(tasks);
+    }
     else
     {
         Console.WriteLine("Invalid choice");
     }
 }
-
-static void AddTask(LinkedList<string> tasks){
+static void AddTask(LinkedList<TodoItem> tasks){
     Console.WriteLine("Write the task:");
-    tasks.AddLast(Console.ReadLine());
+    TodoItem item = new TodoItem(Console.ReadLine());
+    tasks.AddLast(item);
 }
 
-static void DisplayTasks(LinkedList<string> tasks)
+static void DisplayTasks(LinkedList<TodoItem> tasks)
 {
     int count = 0;
-    foreach (string task in tasks)
+    foreach (TodoItem task in tasks)
     {
-        Console.WriteLine("[{0}] {1}", count, task);
+        Console.WriteLine("[{0}] {1} {2}", count, task.Title, task.IsDone ? "o" : "x");
         count++;
     }
 }
 
-static void RemoveTask(LinkedList<string> tasks)
+static void RemoveTask(LinkedList<TodoItem> tasks)
 {
     Console.WriteLine("Which task at which index do you weant to remove");
     int index;
@@ -60,4 +70,32 @@ static void RemoveTask(LinkedList<string> tasks)
     {
         Console.WriteLine("Invalid index");
     }
+}
+
+static void DoTask(LinkedList<TodoItem> tasks)
+{
+    Console.WriteLine("Which task at which index do you want to mark as done?");
+    int index;
+    try { index = int.Parse(Console.ReadLine()); }
+    catch { Console.WriteLine("Invalid index"); return; }
+    int listLenght = tasks.Count;
+    if (index < listLenght)
+    {
+        tasks.ElementAt(index).MarkAsDone();
+    }
+    else
+    {
+        Console.WriteLine("Invalid index");
+    }
+}
+
+static void CompletedTasks(LinkedList<TodoItem> tasks)
+{
+    int count = 0;
+    tasks.Where(task => task.IsDone).ToList().ForEach(task =>
+    {
+        Console.WriteLine("[{0}] {1}", count, task.Title);
+        count++;
+    });
+    Console.WriteLine("Comleted tasks: {0}", count);
 }
---
+++ To-Do List/TodoItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Do_List
+{
+    internal class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+
+        public TodoItem(string title)
+        {
+            Title = title;
+            IsDone = false;
+        }
+
+        public void MarkAsDone()
+        {
+            IsDone = true;
+        }
+    }
+}
